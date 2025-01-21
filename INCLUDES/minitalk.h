aafeb95de91d6cfd0065711ee69d724632df574a
Roux <mzaian@student.42perpignan.fr>
2025-01-21 13:14:59 +0100

    forgot header

--- INCLUDES/minitalk.h
+++ INCLUDES/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: mzaian <mzaian@student.42perpignan.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 11:06:57 by mzaian            #+#    #+#             */
-/*   Updated: 2025/01/15 10:56:22 by mzaian           ###   ########.fr       */
+/*   Updated: 2025/01/18 21:58:36 by mzaian           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,16 +28,26 @@ typedef struct	s_server
 	size_t			msglen;
 	int				keep;
 	int				amountlen;
+	int				good_signature;
+	int				waiting_sig_confirm;
 	int				sigcount;
 	int				sentback;
 }	t_server;
 
 typedef struct	s_client
 {
+	unsigned int	current_bit;
+	unsigned char	mask;
 	unsigned int	ack;
 	int				amount_sent;
-	int				receivedback;
+	int				receive_sig;
+	int				good_signature;
 	pid_t			pid;
 }	t_client;
+
+int	reception_ack(pid_t pid, int sig);
+int	sig_sending(int sig);
+int	char_sending(char c);
+
 
 #endif
