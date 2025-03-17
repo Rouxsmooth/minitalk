fef531881a97c102ef5bd1426d04864b224a2573
Roux <mzaian@student.42perpignan.fr>
2025-03-17 03:31:10 +0100

    added a second testing option

--- Makefile
+++ Makefile
@@ -33,11 +33,11 @@ all: $(LIBNAME)
 
 clean:
 	@$(MAKE) -C $(LIBFT_DIR) clean
-	@rm -f $(OFILES) tester test && echo "$(NAME) object files cleaned."
+	@rm -f $(OFILES) tester && echo "$(NAME) object files cleaned."
 
 fclean:
 	@$(MAKE) -C $(LIBFT_DIR) fclean
-	@rm -f $(OFILES) $(LIBNAME) server client tester test && echo "$(NAME) cleaned.\n"
+	@rm -f $(OFILES) $(LIBNAME) server client tester test test.txt && echo "$(NAME) cleaned.\n"
 
 re: fclean $(LIBNAME)
 
@@ -48,9 +48,17 @@ allc: $(LIBNAME) clean
 
 rec: fclean allc
 
+#./server > test.txtt
 #for i in {1..X} ; do ./client -n PID test$i ; done ;
-tester :
-	@cc $(CFLAGS) SRCS/error_tester.c -o tester $(INCLUDE_LIBFT) && ./tester && rm -f tester
+fortester :
+	@cc $(CFLAGS) SRCS/error_tester.c -o tester $(INCLUDE_LIBFT) && ./tester "for" && rm -f tester
+#`0123456789` n-th times
+nbrstester :
+	@cc $(CFLAGS) SRCS/error_tester.c -o tester $(INCLUDE_LIBFT) && ./tester "nbrs" && rm -f tester
+#START=$(date +%s)
+#./client PID "$(cat FILE)"
+#END=$(date +%s)
+#echo "Execution Time: $((END - START)) seconds
 
 .PHONY:
 	all re tester
--- SRCS/error_tester.c
+++ SRCS/error_tester.c
@@ -6,14 +6,35 @@
 /*   By: mzaian <mzaian@student.42perpignan.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/02 17:44:35 by mzaian            #+#    #+#             */
-/*   Updated: 2025/03/17 02:49:15 by mzaian           ###   ########.fr       */
+/*   Updated: 2025/03/17 03:30:46 by mzaian           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../libft/INCLUDES/libft.h"
 #include "fcntl.h"
 
-int	error_loop(char *line, int serv)
+int	test_nbrs_loop(char *line, int serv)
+{
+	int	i;
+	int	errorcount;
+
+	i = 0;
+	errorcount = 0;
+	while (line[i])
+	{
+		if (line[i] == '\n' && !line[i + 1])
+			return (close(serv), ft_del(line), errorcount);
+		if (line[i] != i % 10 + 48)
+		{
+			errorcount++;
+			ft_printf("issue at index %d | %c : %c\n", i, i % 10 + 48, line[i]);
+		}
+		i++;
+	}
+	return (close(serv), ft_del(line), errorcount);
+}
+
+int	test_i_loop(char *line, int serv)
 {
 	int		i;
 	int		len_without_nl;
@@ -42,13 +63,14 @@ int	error_loop(char *line, int serv)
 	return (close(serv), ft_del(serv_tofind), ft_del(line), errorcount);
 }
 
-int	main(void)
+int	main(int argc, char **argv)
 {
 	char	*line;
 	int		i;
 	int		serv;
 	int		errorcount;
 
+	(void) argc;
 	serv = open("test.txt", O_RDONLY);
 	line = get_next_line(serv);
 	i = 0;
@@ -57,13 +79,21 @@ int	main(void)
 		ft_del(line);
 		line = get_next_line(serv);
 	}
-	errorcount = error_loop(line, serv);
+	if (!ft_strcmp(argv[1], "nbrs"))
+		errorcount = test_nbrs_loop(line, serv);
+	else if (!ft_strcmp(argv[1], "for"))
+		errorcount = test_i_loop(line, serv);
 	if (!errorcount)
-		return (ft_printf("No errors in receiving."), 0);
-	return (ft_printf("There are %d errors\n", errorcount), 0);
+		return (ft_printf("No errors in receiving.\n"), 0);
+	return (ft_printf("There are %d errors.\n", errorcount), 0);
 }
 	/* error output */
 // issue at line 29 | 'test29' : 'test29a'
 // issue at line 47 | 'test47' : 'test42'
 // issue at line 98 | 'test98' : 'test94'
 // There are 3 errors
+
+//issue at index 398 | 8 : 2
+//issue at index 2253 | 3 : 1
+//issue at index 2591 | 1 : d
+//There are 3 errors.
